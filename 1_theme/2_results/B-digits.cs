547a8870848d1d1ece09f588f0938d9d27d196ef
f1rA5h <shtey.alexey@gmail.com>
2021-11-02 19:09:44 +0300

    more and more tasks

---
+++ 1_theme/2_results/B-digits.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Degree {
+    class Program {
+        static void Main(string[] args) {
+        string n = Console.ReadLine();
+
+        Console.WriteLine("{0} {1} {2}", n[0], n[1], n[2]);
+        Console.ReadLine();
+        }
+    }
+}
