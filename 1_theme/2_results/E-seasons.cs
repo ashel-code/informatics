d5fa64bf742b4ba7233b3108c84ffd81662f869c
f1rA5h <shtey.alexey@gmail.com>
2021-11-07 14:31:53 +0300

    e done

--- 1_theme/2_results/E-seasons.cs
+++ 1_theme/2_results/E-seasons.cs
@@ -5,19 +5,23 @@ namespace Degree {
         static void Main(string[] args) { 
             int n = Convert.ToInt32(Console.ReadLine());
 
-            if(n >= 0) 
+            if(n > 0 && n <= 12) 
             {
-                if(n >= 2)
+                if(n <= 2 || n == 12)
                 {
                     Console.WriteLine("winter");
                 }
-                else if(n >= 5)
+                else if(n <= 5)
                 {
                     Console.WriteLine("spring");
                 }
-                else if(n >= 8)
+                else if(n <= 8)
                 {
                     Console.WriteLine("summer");
+                }
+                else 
+                {
+                    Console.WriteLine("autumn");
                 }
             }
             else
